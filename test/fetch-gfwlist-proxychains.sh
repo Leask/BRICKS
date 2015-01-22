adf00d045e0667e0ece8f45f398be7b11a556189
Leask Huang <i@leaskh.com>
2015-01-22 21:15:25 +0800

    [22.7347302566214,114.225229002472] imported from gfwlist

--- test/fetch-gfwlist-proxychains.sh
+++ test/fetch-gfwlist-proxychains.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # dir=${PWD##*/}
 rm ./test/gfwlist.txt
-proxychains4 wget http://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt -O ./test/gfwlist.txt
+proxychains4 wget https://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt -O ./test/gfwlist.txt
