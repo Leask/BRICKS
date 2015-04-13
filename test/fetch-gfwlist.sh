c5fb94e0698b6e94239cea344fec559d558aeea1
Leask Huang <i@leaskh.com>
2015-04-13 15:21:21 +0800

    [22.6066766690227,114.125660475763] updated bricks list

--- test/fetch-gfwlist.sh
+++ test/fetch-gfwlist.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # dir=${PWD##*/}
 rm ./test/gfwlist.txt
-wget https://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt -O ./test/gfwlist.txt
+wget https://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt -O ./test/gfwlist.txt --no-check-certificate
